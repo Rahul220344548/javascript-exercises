99bd80203f56f70dbeec46f876fc06aa038d5922
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-11 21:00:12 +0100

    started function for reversing a string to pass test1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,27 @@
-const reverseString = function () {
+const reverseString = function (word) {
     
+    // Strings in JavaScript cannot be reversed directly so you're going to have
+    // to split it into something else first..do the reversal 
+    // and then join it back together into a string.
     
+    // reverse "hello" => "olleh"
+    
+    let string = ""
+    let a;
+    for (let i = word.length-1; i >= 0; i--) {
+        // console.log(word.charAt(i));
+        a = word.charAt(i);
+        string = string + a
+    }
+
+    
+    return string;
+    
+
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+reverseString("hello");
