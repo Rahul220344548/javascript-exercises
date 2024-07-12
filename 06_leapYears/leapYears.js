a27560e60553b6e6c97e4b4e5c84fc074414bb1c
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-12 20:19:43 +0100

    Finish all tests for leapYears Exerices

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,29 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // 1900 DIVIDE 4 = 475
+    // 475 mod 400 != 0 
+    // 1600 % 4 = 0 && (year / 4) 
+    
+    if (year % 100 ==0) {
+        if (year % 400 ==0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (year % 4==0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
+console.log(leapYears(1900))
+console.log(leapYears(1600));
