6a6e2ed0dea32de4c2766e0129a47bbb0de282fb
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-12 19:34:28 +0100

    completed exericse 05 to pass all tests without any clean coding

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function (num1,num2) {
+    
+    let total = 0;
+
+    if (num1 > num2) {
+        for (let i = num2; i <= num1;i++) {
+            total = total + i;
+        }
+        return total;
+    } 
+    else if (num1<0  || num2 < 0){
+        return "ERROR";
+    }
+    else if (typeof num1 == 'string' || typeof num2 == 'string' ) {
+        return "ERROR";
+    }
+    else if (Array.isArray(num1) || Array.isArray(num2)) {
+        return "ERROR";
+    }
+    else {
+        for (let i = num1; i <= num2;i++) {
+            total = total + i;
+        }
+        return total;
+    }
+    
+    
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+console.log(sumAll(10, [90, 1]))
