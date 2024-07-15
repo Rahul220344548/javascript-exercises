c9fad04fee22d824ef2de19c340792e2db295f0d
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-15 15:01:26 +0100

    completed exerice 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let newTemperature =  ((temperature - 32) * 5 / 9).toFixed(1);
+ 
+  return parseFloat(newTemperature);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let newTemperature = (temperature * 9 / 5 + 32).toFixed(1);
+  return parseFloat(newTemperature);
 };
 
 // Do not edit below this line
