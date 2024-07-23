22619b29cf7d171a4442ace5c4840eb1747b82cb
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-23 18:10:01 +0100

    completed exericse 12 for finding oldest person age with all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+    let sorted = people.sort((a, b) => {
+        const lastPersonAge = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+        const nextPersonAge = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+
+        return nextPersonAge - lastPersonAge;
+    });
+    return sorted[0];
+    
+    
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
