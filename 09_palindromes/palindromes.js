f458392cb03b08d9dc3140ec4a11d353ca8e0f96
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-23 16:03:08 +0100

    completed exericse 09 for palidrones

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let newStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reversed = newStr.split('').reverse().join('').toLowerCase();
+    if (newStr == reversed) return true;
+    return false;
 };
 
 // Do not edit below this line
