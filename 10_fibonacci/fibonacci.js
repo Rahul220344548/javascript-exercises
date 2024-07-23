cda5a882b4f26fda609fdf3b7258b4652628052d
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-23 16:46:30 +0100

    completed fibonacci sequence exericse 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+
+    n = parseInt(n); 
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n == 1) return 1;    
+
+    let fib = [0, 1];
+    for (let i = 2; i <= n; i++) {
+
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[n];
 
 };
 
